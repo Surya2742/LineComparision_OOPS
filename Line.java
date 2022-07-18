1d1cf796d1a7b3668edfd42c5dfa12104fd73588
Surya2742 <boby7724284@gmail.com>
2022-07-19 00:56:58 +0530

    UC2-Equality of 2 Lines

--- Line.java
+++ Line.java
@@ -2,11 +2,14 @@ package LineComparision;
 
 public class Line {
 
-	Point P1, P2;
+	Point P1, P2, P3, P4;
 	int var = 0;
 	
 	public double getLengthLine1(Point P1, Point P2) {
 		return Math.sqrt(Math.pow(P1.x-P2.x, 2)+Math.pow(P1.y-P2.y, 2));
 	}
 	
+	public double getLengthLine2(Point P3, Point P4) {
+		return Math.sqrt(Math.pow(P3.x-P4.x, 2)+Math.pow(P3.y-P4.y, 2));
+	}
 }
--- Main.java
+++ Main.java
@@ -15,14 +15,37 @@ public static void main(String[] args) {
 		System.out.println("Enter x value for point 2");
 		P2.x = ip.inputInteger();
 		System.out.println("Enter y value for point 2");
-		P2.y = ip.inputInteger();	        
+		P2.y = ip.inputInteger();	
+		
+		Point P3 = new Point();
+		System.out.println("Enter x value for point 3");
+		P3.x = ip.inputInteger();
+		System.out.println("Enter y value for point 3");
+		P3.y = ip.inputInteger();
+		
+		Point P4= new Point();
+		System.out.println("Enter x value for point 4");
+		P4.x = ip.inputInteger();
+		System.out.println("Enter y value for point 4");
+		P4.y = ip.inputInteger();
 		
 		Line line1 = new Line();
 		line1.P1 = P1;
 		line1.P2 = P2;
 		
+		Line line2 = new Line();
+		line2.P3 = P3;
+		line2.P4 = P4;
+		
 		Double lineLength1 = line1.getLengthLine1(P1, P2);
+		Double lineLength2 = line1.getLengthLine1(P3, P4);
 		System.out.println("Line 1 Length:" +lineLength1);
+		System.out.println("Line 2 Length:" +lineLength2);
+		
+		if (lineLength1.equals(lineLength2))
+            System.out.println("Line1 and Line2 are equal");
+        else 
+	           System.out.println("Line1 and Line2 are not equal");
  }
 
 }
